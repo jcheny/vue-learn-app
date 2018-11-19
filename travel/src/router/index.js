2bf9ddc17dc5a17ae45df144d33d197d9b436c08
jchen <jchen_px@163.com>
2018-11-19 23:23:48 +0800

    项目初始化完成

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/pages/home/home'
-import List from '@/pages/list/List'
 
 Vue.use(Router)
 
@@ -11,10 +10,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
-    },{
-      path: '/list',
-      name: 'List',
-      component: List
     }
   ]
 })
