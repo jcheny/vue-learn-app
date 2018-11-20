66880a898865ec5bd404ad32d740f4df12c200f5
jchen <jchen_px@163.com>
2018-11-20 22:20:21 +0800

    vue Header 和轮播图的开发

---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" alt="" :src="item.imgUrl"/>
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>    
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data (){
+        return {
+            swiperOption:{
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl: '//img1.qunarzz.com/vc/a5/e0/64/c6bd8680fb67247db15df6057b.jpg'
+            },{
+                id: '0002',
+                imgUrl: '//img1.qunarzz.com/vc/a5/e0/64/c6bd8680fb67247db15df6057b.jpg'
+            }
+            ]
+        }
+    }    
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #ffffff
+    .wrapper 
+        overflow hidden
+        width 100%
+        height 0
+        padding-bottom 31.25%
+        .swiper-img
+            width 100%
+</style>
--- travel/src/pages/home/home.vue
+++ travel/src/pages/home/home.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-       helloworld
+       <home-header></home-header>
+       <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/swiper'
 export default {
-    name: 'home'
+    name: 'home',
+    components: {
+        HomeHeader,
+        HomeSwiper
+    }
 }
 </script>
 
