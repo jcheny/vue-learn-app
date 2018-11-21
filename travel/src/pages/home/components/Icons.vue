8b89173cf30f7604a1fe759a25a150df7574648f
jchen <jchen_px@163.com>
2018-11-21 22:55:48 +0800

    首页开发完成

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -1,72 +1,86 @@
 <template>
     <div class="icons">
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="../../../assets/pic1.png">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
+        <swiper>
+            <swiper-slide v-for="(page,index) of pages" :key="index" >
+                <div class="icon" 
+                v-for="item of page" 
+                :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src='item.imgUrl' />
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 <script>
 export default {
-    name: 'HomeIcons'
+    name: 'HomeIcons',
+    data (){
+        return {
+            iconList: [{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                desc: '景点门票'
+            }, {
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+                desc: '深圳必游'
+            }, {
+                id: '0003',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+                desc: '游乐场'
+            }, {
+                id: '0004',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+                desc: '海洋馆'
+            }, {
+                id: '0005',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+                desc: '动植物园'
+            }, {
+                id: '0006',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+                desc: '泡温泉'
+            }, {
+                id: '0007',
+                imgUrl: 'https://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+                desc: '打卡圣地'
+            }, {
+                id: '0008',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '景点门票'
+            }, {
+                id: '0009',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '景点门票'
+            }]    
+        }
+    },
+    computed: {
+        pages () {
+            const pages = []
+            this.iconList.forEach((item,index) => {
+                const page = Math.floor(index /8)
+                if(!pages[page]){
+                    pages[page] = []
+                }
+                pages[page].push(item)
+            })
+            return pages
+        }
+    }
 }
 </script>
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl';
-    .icons
-        overflow hidden
+    @import '~styles/mixins.styl';
+    .icons >>> .swiper-container
         height 0 
         padding-bottom 50%
+    .icons 
+        margin-bottom .2rem
         .icon
             position relative
             overflow hidden
@@ -95,4 +109,5 @@ export default {
                 line-height .44rem
                 text-align center
                 color $darkTextColor
+                ellipsis()
 </style>
