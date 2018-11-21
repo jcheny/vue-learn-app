e8aa705948e3394e0fb5b11e65babb0327b203ab
jchen <jchen_px@163.com>
2018-11-21 23:37:08 +0800

    首页数据使用axios动态取值，同时优化部分代码

--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper">
-        <swiper :options="swiperOption">
-            <swiper-slide v-for="item of swiperList" :key="item.id">
+        <swiper :options="swiperOption" v-if="showSwiper">
+            <swiper-slide v-for="item of list" :key="item.id">
                 <img class="swiper-img" alt="" :src="item.imgUrl"/>
             </swiper-slide>
             <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,21 +12,22 @@
 <script>
 export default {
     name: 'HomeSwiper',
+    props: {
+        list: Array
+    },
     data (){
         return {
             swiperOption:{
                 pagination: '.swiper-pagination',
                 loop: true
-            },
-            swiperList: [{
-                id: '0001',
-                imgUrl: '//img1.qunarzz.com/vc/a5/e0/64/c6bd8680fb67247db15df6057b.jpg'
-            },{
-                id: '0002',
-                imgUrl: '//img1.qunarzz.com/vc/a4/29/dd/4a0734b77f675da2a8ca4a9879.jpg'
-            }]
+            }
         }
-    }    
+    },
+    computed:{
+        showSwiper(){
+            return this.list.length
+        }
+    }
 }
 </script>
 
