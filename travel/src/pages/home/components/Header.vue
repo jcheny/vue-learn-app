3ed2935bacec6d8858025868a5bb5df25796c613
jchen <jchen_px@163.com>
2018-11-25 16:05:26 +0800

    城市列表也开发完成，有联动效果

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
         <div class="header-input">
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题</div>
+        <router-link to="/city">
         <div class="header-right">
             {{this.city}}
             <span class="iconfont">&#xe64a;</span>
-            </div>
+        </div>
+        </router-link>
     </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
     @import '~styles/varibles.styl';
     .header
         display flex
-        line-height .86rem
+        line-height $headerHeight
         background-color $bgColor
         color #fff
         .header-left
@@ -50,4 +52,5 @@ export default {
             width 1.24rem
             float right
             text-align center
+            color #ffffff
 </style>
